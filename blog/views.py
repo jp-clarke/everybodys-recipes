10b2c25219bf5f6b0f3ec3c432225306b3245477
John Clarke <jp_clarke@outlook.com>
2023-07-26 21:59:15 +0000

    Add comments view to recipe_detail template

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ class RecipeDetail(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
-        item = recipe.ingredients
+        # item = recipe.ingredients
         comments = recipe.comments.filter(approved=True).order_by('date_created')
         favourited = False
         if recipe.favourited.filter(id=self.request.user.id).exists():
@@ -26,7 +26,7 @@ class RecipeDetail(View):
             'recipe_detail.html',
             {
                 'recipe': recipe,
-                'item': item,
+                # 'item': item,
                 'comments': comments,
                 'favourited': favourited
             },
