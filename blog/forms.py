039fc9f4bd961790be506dbee791255f1da5227e
John Clarke <jp_clarke@outlook.com>
2023-07-27 13:36:46 +0000

    Add user commenting functionality

--- blog/forms.py
+++ blog/forms.py
@@ -1,3 +1,4 @@
+from crispy_forms.helper import FormHelper
 from .models import Comment
 from django import forms
 
@@ -6,3 +7,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+    def __init(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_show_labels = False
--- blog/views.py
+++ blog/views.py
@@ -28,6 +28,39 @@ class RecipeDetail(View):
             {
                 'recipe': recipe,
                 'comments': comments,
+                'commented': False,
+                'favourited': favourited,
+                'comment_form': CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(
+            approved=True).order_by('date_created')
+        favourited = False
+        if recipe.favourited.filter(id=self.request.user.id).exists():
+            favourited = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.recipe = recipe
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            'recipe_detail.html',
+            {
+                'recipe': recipe,
+                'comments': comments,
+                'commented': True,
                 'favourited': favourited,
                 'comment_form': CommentForm()
             },
